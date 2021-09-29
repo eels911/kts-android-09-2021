9c6f709d54a739d84f7375213b58cf842b3ea5f4
Sabina <rigby917@gmail.com>
2021-09-29 22:47:33 +0300

    use mvvm

--- app/src/main/java/com/example/android/unsplashmobile/OnboardingFragment.kt
+++ app/src/main/java/com/example/android/unsplashmobile/OnboardingFragment.kt
@@ -6,30 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.android.unsplashmobile.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
-    var binding: FragmentOnboardingBinding? = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentOnboardingBinding.inflate(inflater, container, false)
-        return binding?.root
-    }
+    private val binding: FragmentOnboardingBinding by viewBinding(FragmentOnboardingBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding?.tvSkip?.setOnClickListener {
+        binding.tvSkip.setOnClickListener {
             findNavController().navigate(OnboardingFragmentDirections.actionOnboardingFragmentToLoginFragment())
         }
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
-    }
-
 }
 
